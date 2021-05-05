43da88e38ed2467261b9bbbc061ae232748f59a4
phdinintegrals <phdinintegrals@localhost.localdomain>
2021-05-05 11:22:19 -0400

    В тикет добавлены поля, отражающие текущий статус задачи для наблюдения за ходом работы.

--- projects/common/src/main/java/org/thesis/common/Tickets/CompilationTaskTicket.java
+++ projects/common/src/main/java/org/thesis/common/Tickets/CompilationTaskTicket.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 
 public @Data
 class CompilationTaskTicket implements java.io.Serializable {
+    //Desciptive attributes
     String processingServerName;
     long ID;
     String projectName;
@@ -15,8 +16,15 @@ class CompilationTaskTicket implements java.io.Serializable {
     boolean[] necessaryStages;
     boolean[] currentStages;
     
+    //Result attributes
     boolean error;
     int lastStage;
+    
+    //State attributes
+    STAGES currentStage;
+    STATES currentState;
+    String hostname;
+    int estTime;
 
      public CompilationTaskTicket(){
          ID = 0L;
@@ -29,6 +37,11 @@ class CompilationTaskTicket implements java.io.Serializable {
          Arrays.fill(currentStages,false);
          error = false;
          lastStage = 0;
+         
+         currentStage = STAGES.NAN;
+         currentState = STATES.IDLE;
+         hostname = "";
+         estTime = 0;
      }
 
     public CompilationTaskTicket(String _processingServerName,long _ID, String _projectName, String _projectPath, int lastNecessaryStage){
@@ -42,12 +55,37 @@ class CompilationTaskTicket implements java.io.Serializable {
         Arrays.fill(currentStages,false);
         error = false;
         lastStage = 0;
+        
+         currentStage = STAGES.NAN;
+         currentState = STATES.IDLE;
+         hostname = "";
+         estTime = 0;
     }
 
      public String getUUID(){
          Long lID = ID;
          String res = processingServerName +"-" + projectName + "-" + lID.toString();
          return res;
+     }
+     
+     public int getNextStageIndex(){
+        int taskStage = -1;
+        for(int i=0; i< currentStages.length && taskStage == -1; i++){
+            System.out.println( "\t" + currentStages[i] + "/"+ necessaryStages[i]);
+            if( !currentStages[i] && necessaryStages[i]){
+                taskStage = i;
+            }
+        }
+        return taskStage;
+     }
+     
+     public STAGES getNextStage(){
+        int taskStage = getNextStageIndex();
+        if( taskStage == -1){
+            return STAGES.NAN;
+        }
+        return STAGES.values()[taskStage];
+        
      }
 
 }
---
+++ projects/common/src/main/java/org/thesis/common/Tickets/STATES.java
@@ -0,0 +1,8 @@
+package org.thesis.common.Tickets;
+
+public enum STATES {
+   IDLE,
+   PROCESSING,
+   QUEUED,
+   FINISHED
+} 
