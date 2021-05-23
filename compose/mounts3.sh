2e7b2664e28ccd48009bf3a507547dcbbe0e1071
phdinintegrals <phdinintegrals@localhost.localdomain>
2021-05-23 10:19:44 -0400

    ПРава для директории s3

--- compose/mounts3.sh
+++ compose/mounts3.sh
@@ -1,1 +1,6 @@
-s3fs src ./mount0 -o passwd_file=~/.passwd-s3fs -o url=http://localhost:9000/ -o use_path_request_style
+
+export HOME=/home/phdinintegrals
+mkdir /tmp/s3mount0
+echo minioadmin:minioadmin > ${HOME}/.passwd-s3fs
+chmod 600 ${HOME}/.passwd-s3fs
+s3fs src /tmp/s3mount0 -o passwd_file=${HOME}/.passwd-s3fs -o url=http://localhost:9000 -o use_path_request_style -o umask=0000
